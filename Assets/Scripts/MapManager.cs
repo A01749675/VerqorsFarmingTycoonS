4fddc649c1866979c159e0e16e2e9755328e694e
Iker Fuentes <A01749675@tec.mx>
2024-03-08 11:39:53 -0600

    Update

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -42,18 +42,18 @@ public class MapManager : MonoBehaviour
     {
     if(Input.GetMouseButtonDown(0))
         {
-            
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3Int gridPos = tilemap.WorldToCell(mousePos);
             TileBase clickedTile = tilemap.GetTile(gridPos);
-            if(dataFromTiles.ContainsKey(clickedTile)){
+            if(clickedTile && dataFromTiles.ContainsKey(clickedTile)){
                 selected_crop = dataFromTiles[clickedTile].crop_type;
-                print("Current crop: "+selected_crop);
+                print("Selected: " + selected_crop);
             }
 
         }
 
     if(Input.GetKeyDown("p")){
+
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             PlantCrop(mousePos);
         }
@@ -69,27 +69,32 @@ public class MapManager : MonoBehaviour
             return;
         }
         TileBase tile = tilemap.GetTile(gridPosition);
-        if(dataFromTiles.ContainsKey(tile) && dataFromTiles[tile].crop_type==-1){
-            switch(selected_crop){
-                case 1:
-                    tilemap.SetTile(gridPosition, barley_seeds);
-                    break;
-                case 2:
-                    tilemap.SetTile(gridPosition, corn_seeds);
-                    break;
-                case 3:
-                    tilemap.SetTile(gridPosition, tomato_seeds);
-                    break;
-                case 4:
-                    tilemap.SetTile(gridPosition, avocado_seeds);
-                    break;
-                case 5:
-                    tilemap.SetTile(gridPosition, coffee_seeds);
-                    break;
-                case 6: 
-                    tilemap.SetTile(gridPosition, chili_seeds);
-                    break;   
+        if(dataFromTiles.ContainsKey(tile)){
+            print("Key found");
+            if(dataFromTiles[tile].crop_type==-1){
+                print("Selecting crop");
+                switch(selected_crop){
+                        case 1:
+                            PlantAll(barley_seeds,selected_crop);
+                            break;
+                        case 2:
+                            PlantAll(corn_seeds,selected_crop);
+                            break;
+                        case 3:
+                            PlantAll(tomato_seeds,selected_crop);
+                            break;
+                        case 4:
+                            PlantAll(avocado_seeds,selected_crop);
+                            break;
+                        case 5:
+                            PlantAll(coffee_seeds,selected_crop);
+                            break;
+                        case 6: 
+                            PlantAll(chili_seeds,selected_crop);
+                            break;   
+                    }
             }
+            print("Planted");
             cropManager.UpdateCropSeeds(selected_crop, -1);
         }
         
@@ -163,11 +168,15 @@ public class MapManager : MonoBehaviour
         int Water = dataFromTiles[tile].water;
         return Water;
     }
-    public void PlantCorn(){
-        foreach(var tileData in tileDatas){
-            foreach(var tile in tileData.tiles){
-                if(tileData.crop_type==0){
-                    
+    public void PlantAll(TileBase seed, int crop_type){
+        int i = 0;
+        for(i = 0; i<tilemap.size.x; i++){
+            for(int j = 0; j<tilemap.size.y; j++){
+                Vector3Int gridPosition = new Vector3Int(i, j, 0);
+                TileBase tile = tilemap.GetTile(gridPosition);
+                if(tile && dataFromTiles.ContainsKey(tile) && dataFromTiles[tile].crop_type==-1){
+                    tilemap.SetTile(gridPosition, seed);
+                    cropManager.UpdateCropSeeds(crop_type, -1);
                 }
             }
         }
