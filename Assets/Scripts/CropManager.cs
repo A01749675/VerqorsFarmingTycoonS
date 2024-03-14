e89082dd63e82de25b3ac29c69a39fb3f3998e57
Iker Fuentes <A01749675@tec.mx>
2024-03-13 20:16:31 -0600

    Small update

--- Assets/Scripts/CropManager.cs
+++ Assets/Scripts/CropManager.cs
@@ -30,8 +30,6 @@ public class CropManager : MonoBehaviour
         };
         }
 
-    
-
     public void UpdateCropQuantity(int cropType, int quantity){
         crop_quantity[cropType] += quantity;
     }
