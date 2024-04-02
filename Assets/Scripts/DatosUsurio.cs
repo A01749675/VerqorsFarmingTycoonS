893425e31b496072c2162d04e9b8f39b5b6c45bb
Alan Vega <a01750658@tec.mx>
2024-04-01 19:24:07 -0600

    Cambios periódico dinámico

--- Assets/Scripts/DatosUsurio.cs
+++ Assets/Scripts/DatosUsurio.cs
@@ -7,10 +7,28 @@ public class DatosUsurio : MonoBehaviour
 {
     public GameObject NombreUsuario;
     public GameObject TipoFinanciamiento;
+    public ObtenerDatos obtenerDatos;
+    private string nombreUsuario;
+    private int financiamiento;
 
     // Start is called before the first frame update
     void Start(){
-        NombreUsuario.GetComponent<TextMeshProUGUI>().text = "Marzy";
-        TipoFinanciamiento.GetComponent<TextMeshProUGUI>().text = "Coyote";
+        nombreUsuario = obtenerDatos.usuario.usuario;
+        financiamiento = obtenerDatos.progreso[0].financiamiento;
+        switch (financiamiento){
+            case 1:
+                TipoFinanciamiento.GetComponent<TextMeshProUGUI>().text = "Verqor";
+                break;
+            case 2:
+                TipoFinanciamiento.GetComponent<TextMeshProUGUI>().text = "Banco";
+                break;
+            case 3:
+                TipoFinanciamiento.GetComponent<TextMeshProUGUI>().text = "Coyote";
+                break;
+            default:
+                TipoFinanciamiento.GetComponent<TextMeshProUGUI>().text = "-";
+                break;
+        }
+        NombreUsuario.GetComponent<TextMeshProUGUI>().text = nombreUsuario;
     }
 }
