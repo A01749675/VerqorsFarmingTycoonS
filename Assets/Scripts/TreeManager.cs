030f7c295ca20e3f479bbde48a0152e5a2083f74
SantiagoChevez <a01749887@tec.mx>
2024-04-11 17:11:44 -0600

    Mejora visual al arbol

--- Assets/Scripts/TreeManager.cs
+++ Assets/Scripts/TreeManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
 public class TreeManager : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class TreeManager : MonoBehaviour
     private GameObject Texto;
     private Dictionary<int,bool> Mejoras = new Dictionary<int, bool>();
     private GameObject BotonComprar;
+    private GameObject BotonInfo;
     private int TFin;
     public GameObject Reg1;
     public GameObject Reg2;
@@ -19,12 +21,24 @@ public class TreeManager : MonoBehaviour
     public GameObject Seg2;
     public GameObject Seg3;
     private int seleccion;
+    public Image Imagen;
+    public Sprite Tractor;
+    public Sprite Empleado;
+    public Sprite Tierra;
+    public Sprite Aspersor;
+    public Sprite Tanque;
+    public Sprite Agua;
+    public Sprite Seguro;
+    public Sprite IMRegen;
+
+
 
     public void Awake()
     {
         Texto=Arbol.transform.GetChild(0).GetChild(1).gameObject;
         BotonComprar = Arbol.transform.GetChild(0).GetChild(2).gameObject;
-        TFin = 2;
+        BotonInfo = Arbol.transform.GetChild(0).GetChild(3).gameObject;
+        TFin = 1;
         if(false){
             print("Base de datos");
         } else{
@@ -72,6 +86,7 @@ public class TreeManager : MonoBehaviour
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 1000";
+        Imagen.sprite = Agua;
         if (!Mejoras[1])
         {
             BotonComprar.SetActive(true);
@@ -80,12 +95,14 @@ public class TreeManager : MonoBehaviour
         
         }
         seleccion = 1;
+        BotonInfo.SetActive(false);
         
     }
     public void ShowUpgrade2()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 2000";
+        Imagen.sprite = Tierra;
         if (Mejoras[1] && !Mejoras[2])
         {
             BotonComprar.SetActive(true);
@@ -94,11 +111,13 @@ public class TreeManager : MonoBehaviour
         
         }
         seleccion = 2;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade3()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 3000";
+        Imagen.sprite = Empleado;
         if (Mejoras[2] && !Mejoras[3])
         {
             BotonComprar.SetActive(true);
@@ -107,11 +126,13 @@ public class TreeManager : MonoBehaviour
         
         }
         seleccion = 3;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade4()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 4000";
+        Imagen.sprite = Tractor;
         if (Mejoras[2] && !Mejoras[4])
         {
             BotonComprar.SetActive(true);
@@ -120,11 +141,13 @@ public class TreeManager : MonoBehaviour
         
         }
         seleccion = 4;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade5()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 5000";
+        Imagen.sprite = Tanque;
         if (Mejoras[2] && !Mejoras[5])
         {
             BotonComprar.SetActive(true);
@@ -133,11 +156,13 @@ public class TreeManager : MonoBehaviour
         
         }
         seleccion = 5;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade6()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 6000";
+        Imagen.sprite = Seguro;
         if (Mejoras[2] && !Mejoras[6])
         {
             BotonComprar.SetActive(true);
@@ -145,11 +170,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 6;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade7()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 7000";
+        Imagen.sprite = Aspersor;
         if (Mejoras[3] && Mejoras[4] && Mejoras[5] && !Mejoras[7])
         {
             BotonComprar.SetActive(true);
@@ -157,11 +184,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 7;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade8()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 8000";
+        Imagen.sprite = IMRegen;
         if (Mejoras[3] && Mejoras[4] && Mejoras[5] && !Mejoras[8])
         {
             BotonComprar.SetActive(true);
@@ -169,11 +198,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 8;
+        BotonInfo.SetActive(true);
     }
     public void ShowUpgrade9()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 9000";
+        Imagen.sprite = Empleado;
         if (Mejoras[7] && Mejoras[8] && !Mejoras[9])
         {
             BotonComprar.SetActive(true);
@@ -181,11 +212,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 9;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade10()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 10000";
+        Imagen.sprite = Tractor;
         if (Mejoras[7] && Mejoras[8] && !Mejoras[10])
         {
             BotonComprar.SetActive(true);
@@ -193,11 +226,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 10;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade11()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 11000";
+        Imagen.sprite = Tanque;
         if (Mejoras[7] && Mejoras[8] && !Mejoras[11])
         {
             BotonComprar.SetActive(true);
@@ -205,11 +240,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 11;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade12()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 12000";
+        Imagen.sprite = Seguro;
         if (Mejoras[7] && Mejoras[8] && Mejoras[6] && !Mejoras[12])
         {
             BotonComprar.SetActive(true);
@@ -217,11 +254,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 12;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade13()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 13000";
+        Imagen.sprite = Tierra;
         if (Mejoras[9] && Mejoras[10] && Mejoras[11] && !Mejoras[13])
         {
             BotonComprar.SetActive(true);
@@ -229,11 +268,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 13;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade14()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 14000";
+        Imagen.sprite = Aspersor;
         if (Mejoras[9] && Mejoras[10] && Mejoras[11] && !Mejoras[14])
         {
             BotonComprar.SetActive(true);
@@ -241,11 +282,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 14;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade15()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 15000";
+        Imagen.sprite = IMRegen;
         if (Mejoras[9] && Mejoras[10] && Mejoras[11] && !Mejoras[15])
         {
             BotonComprar.SetActive(true);
@@ -253,11 +296,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 15;
+        BotonInfo.SetActive(true);
     }
     public void ShowUpgrade16()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 16000";
+        Imagen.sprite = Empleado;
         if (Mejoras[13] && Mejoras[14] && Mejoras[15] && !Mejoras[16])
         {
             BotonComprar.SetActive(true);
@@ -265,11 +310,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 16;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade17()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 17000";
+        Imagen.sprite = Tractor;
         if (Mejoras[13] && Mejoras[14] && Mejoras[15] && !Mejoras[17])
         {
             BotonComprar.SetActive(true);
@@ -277,11 +324,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 17;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade18()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 18000";
+        Imagen.sprite = Tanque;
         if (Mejoras[13] && Mejoras[14] && Mejoras[15]  && !Mejoras[18])
         {
             BotonComprar.SetActive(true);
@@ -289,11 +338,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 18;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade19()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 19000";
+        Imagen.sprite = Seguro;
         if (Mejoras[13] && Mejoras[14] && Mejoras[15] && Mejoras[12] && !Mejoras[19])
         {
             BotonComprar.SetActive(true);
@@ -301,11 +352,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 19;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade20()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 20000";
+        Imagen.sprite = Tierra;
         if (Mejoras[16] && Mejoras[17] && Mejoras[18] && !Mejoras[20])
         {
             BotonComprar.SetActive(true);
@@ -313,11 +366,13 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 20;
+        BotonInfo.SetActive(false);
     }
     public void ShowUpgrade21()
     {
         Arbol.transform.GetChild(0).gameObject.SetActive(true);
         Texto.GetComponent<TextMeshProUGUI>().text = "Costo: 21000";
+        Imagen.sprite = IMRegen;
         if (Mejoras[16] && Mejoras[17] && Mejoras[18] && !Mejoras[21])
         {
             BotonComprar.SetActive(true);
@@ -325,6 +380,7 @@ public class TreeManager : MonoBehaviour
             BotonComprar.SetActive(false);
         }
         seleccion = 21;
+        BotonInfo.SetActive(true);
     }
 
     public void Comprar(){
