ee616f3aa0b5b8f3d02e816d8ec89cad01dca33d
Iker Fuentes <A01749675@tec.mx>
2024-04-12 10:20:59 -0600

    A lot of updates I don't wanna talk about

--- Assets/Scripts/EmployeeManager.cs
+++ Assets/Scripts/EmployeeManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 
 public class EmployeeManager : MonoBehaviour
 {
+    public static System.Random random = new System.Random();
 
     [SerializeField]
     private int assigned_land = -1;
@@ -12,16 +14,42 @@ public class EmployeeManager : MonoBehaviour
     private int assigned_crop = -1;
 
     private int last_plant = 0;
+    private int last_movement = 0;
+
+    private int current_movement = 0;
+
+    private int movement_constant = 5;
+    private int planting_constant = 10;
+
+    private int show_emotion_time = 10;
+
+    private int show_emotion_cycle = 20;
+
+    private int showed_emotion = 0;
+
+    private bool unlocked_farmer = true;
+
+    [SerializeField]
+    private List<Sprite> farmer_emotion;
 
     [SerializeField]
     private MapManager mapManager;
+    [SerializeField]
+    private CropManager cropManager;
 
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private Vector3 initial_position;
+
+    private bool flip = false;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        initial_position = transform.position;
         //AssignLandToEmployee(12);
         
     }
@@ -43,29 +71,67 @@ public class EmployeeManager : MonoBehaviour
     }
 
     public void PlantLand(){
-        if(assigned_land != -1){
+        if(assigned_land != -1 && !mapManager.LandPlanted(assigned_land) && cropManager.GetCropSeeds(assigned_crop) > 0){
             mapManager.FarmerAutomaticPlanting(assigned_land,assigned_crop);
             animator.SetBool("ActivatedMovement",true);
         }
     
     }
+
+    public void ShowEmotion(){
+        GameObject child = gameObject.transform.GetChild(0).gameObject;
+        child.SetActive(true);
+        child.GetComponent<SpriteRenderer>().sprite = farmer_emotion[random.Next(0,farmer_emotion.Count-1)];
+
+    }
+
+    public void HideEmotion(){
+        GameObject child = gameObject.transform.GetChild(0).gameObject;
+        child.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
         int cycle = mapManager.GetCurrentCycle();
-        
-        if(cycle % 10 == 0 && mapManager.LandPlanted(assigned_land) == false && assigned_land != -1){
-            assigned_crop = mapManager.GetCropAtLand(assigned_land);
-            print("Employee has been called to work at " +assigned_land+ " with crop "+ assigned_crop);
-            print("Employee Tried to plant");
-            PlantLand();
-            last_plant = cycle;
+        if(unlocked_farmer){
+            if(cycle % 20 == 0 && mapManager.LandPlanted(assigned_land) == false && assigned_land != -1){
+                assigned_crop = mapManager.GetCropAtLand(assigned_land);
+                PlantLand();
+                last_plant = cycle;
+                last_movement = cycle;
+
+            }
+            if(cycle - last_plant > planting_constant && last_plant != 0){
+                animator.SetBool("ActivatedMovement",false);
+                last_plant = 0;
+            }
+            if((cycle-last_movement)<movement_constant && last_movement != 0){
+                transform.position += Vector3.right*Time.deltaTime;
+                current_movement = cycle;
+                
+            }
+            else if((cycle-current_movement)<movement_constant && current_movement != 0){
+                spriteRenderer.flipX = !flip;
+                transform.position -= Vector3.right*Time.deltaTime;
+            }
+            else{
+                if(current_movement != 0){
+                    flip = !flip;
+                    spriteRenderer.flipX = flip;
+                }
+                last_movement=0;
+                current_movement=0;
+                transform.position = initial_position;
+            }
+            
+            if(cycle%show_emotion_cycle == 0){
+                ShowEmotion();
+                showed_emotion = cycle;
+            }
+            else if ((cycle-showed_emotion)%show_emotion_time == 0 && showed_emotion != 0){
+                HideEmotion();
+            }
         }
-        if(cycle - last_plant > 10 && last_plant != 0){
-            animator.SetBool("ActivatedMovement",false);
-            last_plant = 0;
-        }
-        
         
     }
 }
