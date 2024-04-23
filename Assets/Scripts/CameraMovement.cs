faa2c472fd67de9205005c9b2205b59ce66c6746
Alan Vega <a01750658@tec.mx>
2024-04-23 10:44:54 -0600

    CameraLimit

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -8,8 +9,18 @@ public class CameraMovement : MonoBehaviour
     private Camera cam;
 
     [SerializeField]
-    private float zoomStep, minCamSize, maxCamSize;
+    private SpriteRenderer mapRenderer;
+
+    private float mapMinX, mapMaxX, mapMinY, mapMaxY;
     private Vector3 dragOrigin;
+
+    private void Awake(){
+        mapMinX = mapRenderer.transform.position.x - mapRenderer.bounds.size.x/2f;
+        mapMaxX = mapRenderer.transform.position.x + mapRenderer.bounds.size.x/2f;
+        mapMinY = mapRenderer.transform.position.y - mapRenderer.bounds.size.y/2f;
+        mapMaxY = mapRenderer.transform.position.y + mapRenderer.bounds.size.y/2f;
+
+    }
     private void Update(){
         
         PanCamera();
@@ -25,10 +36,24 @@ public class CameraMovement : MonoBehaviour
             Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
 
             
-
-            cam.transform.position += difference;
+            cam.transform.position = ClampCamera(cam.transform.position + difference);
         }
 
+    }
+
+    private Vector3 ClampCamera(Vector3 targetPosition){
+        float camHeight = cam.orthographicSize;
+        float camWidth = cam.orthographicSize * cam.aspect;
+
+        float minX = mapMinX + camWidth;
+        float maxX = mapMaxX - camWidth;
+        float minY = mapMinY + camHeight;
+        float maxY = mapMaxY - camHeight;
+
+        float newX = Mathf.Clamp(targetPosition.x, minX, maxX);
+        float newY = Mathf.Clamp(targetPosition.y, minY, maxY);
+
+        return new Vector3(newX, newY, targetPosition.z);
     }
 
 }
