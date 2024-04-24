39bf1538d0036195552860ddadb18052122c9ba6
Mariana Marzayani Hernández Jurado <133036569+MarzyHernandez@users.noreply.github.com>
2024-04-24 13:33:53 -0600

    Delete InterfazWeb directory

--- InterfazWeb/script.js
+++
@@ -1,7 +0,0 @@
-function toggleForms() {
-    var registerForm = document.getElementById("register-form");
-    var loginForm = document.getElementById("login-form");
-
-    registerForm.classList.toggle("hidden");
-    loginForm.classList.toggle("hidden");
-}
