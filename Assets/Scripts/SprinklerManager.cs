ec1c544268be2ee2cdce0a3af6cf0fdee7435fae
Iker Fuentes <A01749675@tec.mx>
2024-04-29 14:32:18 -0600

    Update in sprinklers

--- Assets/Scripts/SprinklerManager.cs
+++ Assets/Scripts/SprinklerManager.cs
@@ -22,13 +22,13 @@ public class SprinklerManager : MonoBehaviour
 
     private bool flag = false;
     public void WaterCrops(){
-        if(mapManager.GetCurrentCycle()%cycle==0 && !flag){
+        
+        if(mapManager.GetCurrentCycle()%cycle==0 && !flag && mapManager.GetCropsInLand(land_id)>0){
             //print(tankManager.GetWaterLevel());
             if(mapManager.GetAverageWaterAtLand(land_id) < 75 && tankManager.GetWaterLevel() > 0 && mapManager.LandPlanted(land_id)){
                 animator.enabled = true;
                 mapManager.WaterSpecificLand(land_id);
                 tankManager.SetWaterLevel(-5);
-                print("Yeet: The tank has: "+tankManager.GetWaterLevel());
                 flag = true;
             }
         }
@@ -37,6 +37,10 @@ public class SprinklerManager : MonoBehaviour
             spriteRenderer.sprite = initial;
         }
         if(tankManager.GetWaterLevel() <= 0){
+            animator.enabled = false;
+            spriteRenderer.sprite = initial;
+        }
+        if(mapManager.GetCropsInLand(land_id)==0){
             animator.enabled = false;
             spriteRenderer.sprite = initial;
         }
