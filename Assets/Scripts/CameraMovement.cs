36a085b39a559280bd514067916260520336c634
Iker Fuentes <A01749675@tec.mx>
2024-04-29 14:09:58 -0600

    Added wasd to camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -27,22 +27,22 @@ public class CameraMovement : MonoBehaviour
         PanCamera();
 
 
-    if(Input.GetKey(KeyCode.RightArrow))
+    if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
 	{
 		transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
         cam.transform.position = ClampCamera(cam.transform.position);
 	}
-	if(Input.GetKey(KeyCode.LeftArrow))
+	if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
 	{
 		transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
         cam.transform.position = ClampCamera(cam.transform.position);
 	}
-	if(Input.GetKey(KeyCode.DownArrow))
+	if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
 	{
 		transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
         cam.transform.position = ClampCamera(cam.transform.position);
 	}
-	if(Input.GetKey(KeyCode.UpArrow))
+	if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
 	{
 		transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
         cam.transform.position = ClampCamera(cam.transform.position);
