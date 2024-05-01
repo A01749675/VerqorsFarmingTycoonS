7a129ddb143472eef66c3058387da6b95bfc17f0
Marzy <A01799263@tec.mx>
2024-04-30 21:16:19 -0600

    Necessary web updates

--- Assets/Scripts/EnviarDatos.cs
+++ Assets/Scripts/EnviarDatos.cs
@@ -140,7 +140,7 @@ public class EnviarDatos : MonoBehaviour
     // corrutina de enviar datos
     private IEnumerator EnviarDatosUsuario(string jsonData)
     {
-        string apiUrl = "http://localhost:3000/game-data";
+        string apiUrl = "http://52.5.57.146:8080/game-data";
 
         // Crear un objeto UnityWebRequest para enviar los datos
         UnityWebRequest www = new UnityWebRequest(apiUrl, "POST");
@@ -164,7 +164,7 @@ public class EnviarDatos : MonoBehaviour
     }
     private IEnumerator EnviarDatosUsuario2(string jsonData)
     {
-        string apiUrl = "http://localhost:3000/game-data";
+        string apiUrl = "hhttp://52.5.57.146:8080/game-data";
 
         // Crear un objeto UnityWebRequest para enviar los datos
         UnityWebRequest www = new UnityWebRequest(apiUrl, "POST");
@@ -188,7 +188,7 @@ public class EnviarDatos : MonoBehaviour
             UnityWebRequest www3 = UnityWebRequest.Get(redirectUrl);
             yield return www3.SendWebRequest(); */
 
-            Application.OpenURL("http://localhost:3000/");
+            Application.OpenURL("http://52.5.57.146:8080/");
         }
     }
 
